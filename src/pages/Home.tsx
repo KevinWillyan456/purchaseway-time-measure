6014a7fdc6121a6b3174f06c9dd08e1c8a3b1564
Kevin Souza <kevinwillyansouza@gmail.com>
2024-07-26 11:17:08 -0400

    feat: criação dos componentes da página home (em desenvolvimento)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,49 @@
-import { IonPage } from '@ionic/react'
+import {
+    IonButton,
+    IonDatetime,
+    IonDatetimeButton,
+    IonModal,
+    IonPage,
+} from '@ionic/react'
+import './Home.css'
 
 const Home: React.FC = () => {
-    return <IonPage>Hello World</IonPage>
+    return (
+        <IonPage>
+            <section className="home-container">
+                <h1 className="header">Calculadora de Intervalo de Tempo</h1>
+
+                <div className="form-group">
+                    <p>Intervalo de início</p>
+
+                    <div className="datetime-wrapper">
+                        <IonDatetimeButton datetime="datetime-start"></IonDatetimeButton>
+                    </div>
+
+                    <IonModal keepContentsMounted={true}>
+                        <IonDatetime id="datetime-start"></IonDatetime>
+                    </IonModal>
+                </div>
+
+                <div className="form-group">
+                    <p>Intervalo de término</p>
+
+                    <div className="datetime-wrapper">
+                        <IonDatetimeButton datetime="datetime-end"></IonDatetimeButton>
+                    </div>
+
+                    <IonModal keepContentsMounted={true}>
+                        <IonDatetime id="datetime-end"></IonDatetime>
+                    </IonModal>
+                </div>
+
+                <IonButton shape="round">Calcular</IonButton>
+
+                <div className="result">O tempo entre os intervalos é: </div>
+                <span id="resultado">1h</span>
+            </section>
+        </IonPage>
+    )
 }
 
 export default Home
