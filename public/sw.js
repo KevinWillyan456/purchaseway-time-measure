25fd4b9754520c08fc545be2bd73add7b60d6eea
Kevin Souza <kevinwillyansouza@gmail.com>
2026-08-24 15:29:34 -0400

    feat: adicionar suporte a PWA com manifest e service worker

---
+++ public/sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'pw-time-v1'
+const urlsToCache = ['/', '/index.html']
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)))
+})
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request)
+    }),
+  )
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name)),
+      )
+    }),
+  )
+})
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,12 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+  })
+}
+
 const container = document.getElementById('root')
 const root = createRoot(container!)
 root.render(
